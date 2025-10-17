f808740a522a74f8e6b2171177e32e0e7c00991e
Alana Queiroz <166722191+alanaqroz@users.noreply.github.com>
2025-10-17 19:04:15 -0300

    Update Elemento.java
    adicionando construtor

--- Elemento.java
+++ Elemento.java
@@ -1,6 +1,12 @@
 public class Elemento {
     String id;
     String descricao;
+    String dataHora;
 
-
+    // Construtor para solicitações de serviço e clientes
+    public Elemento(String id, String descricao, String dataHora) {
+        this.id = id;
+        this.descricao = descricao;
+        this.dataHora = dataHora; 
+    }
 }
